57e7f8e526be45f0c3fa4de50d75889548090b06
kingsley <KingsleyAkpan18@gmail.com>
2018-10-02 21:58:13 -0400

    Created Components

---
+++ AngularPractice/src/Component/SelfCreated-component/selfCreate-component.component.ts
@@ -0,0 +1,15 @@
+import {Component,OnInit } from '@angular/core';
+
+@Component({
+selector: "selfCreate",
+templateUrl: "./selfCreate-Component.component.html",
+})
+
+export class SelfCrateComponent implements OnInit {
+
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
+  }
--- AngularPractice/src/app/app.module.ts
+++ AngularPractice/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from '../Component/test-component/test-component.component';
+import {SelfCrateComponent} from '../Component/SelfCreated-component/selfCreate-component.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    SelfCrateComponent
   ],
   imports: [
     BrowserModule
